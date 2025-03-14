7b3c70f5df448da2b4632a36cc1d70d85535e1b8
Molerodev <alejandromolero.developer@gmail.com>
2025-03-14 19:29:04 +0100

    Lista de Tareas (Faltan Mejoras)

--- curso-angular-yt/src/app/app.component.ts
+++ curso-angular-yt/src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TareasService } from './services/tareas.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'curso-angular-yt';
+export class AppComponent implements OnInit {
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  private _tareasService = inject(TareasService);
+
+
+  ngOnInit() {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  agregarTarea() {
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.
+
+    listaTareas = this._tareasService.getTareas();
+  }
+
+  eliminarTarea(index: number) {
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
 }
